af09aa7f8e3323effa36e153978eac260f6952a5
Gabriel Santos Faria <fariamg4@gmail.com>
2025-10-28 15:57:20 -0300

    feat: refactor PrismaService to use private _extendedClient for sensitive data omission

--- meu-projeto-doacao/apps/user-service/src/prisma.service.ts
+++ meu-projeto-doacao/apps/user-service/src/prisma.service.ts
@@ -1,6 +1,11 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma';
 
+/**
+ * PrismaService - Serviço de Banco de Dados
+ * Configurado com Prisma Client Extension para automaticamente omitir campos sensíveis
+ * (passwordHash, twoFactorSecret, twoFactorBackupCodes) em todas as queries de usuário
+ */
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
@@ -11,57 +16,64 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
-  get extended() {
-    return this.$extends({
-      query: {
-        user: {
-          async create({ args, query }) {
-            args.omit = {
-              passwordHash: true,
-              twoFactorSecret: true,
-              twoFactorBackupCodes: true,
-              ...args.omit,
-            };
-            return query(args);
-          },
-          async update({ args, query }) {
-            args.omit = {
-              passwordHash: true,
-              twoFactorSecret: true,
-              twoFactorBackupCodes: true,
-              ...args.omit,
-            };
-            return query(args);
-          },
-          async findUnique({ args, query }) {
-            args.omit = {
-              passwordHash: true,
-              twoFactorSecret: true,
-              twoFactorBackupCodes: true,
-              ...args.omit,
-            };
-            return query(args);
-          },
-          async findFirst({ args, query }) {
-            args.omit = {
-              passwordHash: true,
-              twoFactorSecret: true,
-              twoFactorBackupCodes: true,
-              ...args.omit,
-            };
-            return query(args);
-          },
-          async findMany({ args, query }) {
-            args.omit = {
-              passwordHash: true,
-              twoFactorSecret: true,
-              twoFactorBackupCodes: true,
-              ...args.omit,
-            };
-            return query(args);
-          },
+  /**
+   * Client estendido com omissão automática de campos sensíveis
+   * Este é o client padrão que deve ser usado em toda a aplicação
+   * para garantir que dados sensíveis nunca sejam expostos acidentalmente
+   */
+  private _extendedClient = this.$extends({
+    query: {
+      user: {
+        async create({ args, query }) {
+          args.omit = {
+            passwordHash: true,
+            twoFactorSecret: true,
+            twoFactorBackupCodes: true,
+            ...args.omit,
+          };
+          return query(args);
+        },
+        async update({ args, query }) {
+          args.omit = {
+            passwordHash: true,
+            twoFactorSecret: true,
+            twoFactorBackupCodes: true,
+            ...args.omit,
+          };
+          return query(args);
+        },
+        async findUnique({ args, query }) {
+          args.omit = {
+            passwordHash: true,
+            twoFactorSecret: true,
+            twoFactorBackupCodes: true,
+            ...args.omit,
+          };
+          return query(args);
+        },
+        async findFirst({ args, query }) {
+          args.omit = {
+            passwordHash: true,
+            twoFactorSecret: true,
+            twoFactorBackupCodes: true,
+            ...args.omit,
+          };
+          return query(args);
+        },
+        async findMany({ args, query }) {
+          args.omit = {
+            passwordHash: true,
+            twoFactorSecret: true,
+            twoFactorBackupCodes: true,
+            ...args.omit,
+          };
+          return query(args);
         },
       },
-    });
+    },
+  });
+
+  get user() {
+    return this._extendedClient.user as any;
   }
 }
