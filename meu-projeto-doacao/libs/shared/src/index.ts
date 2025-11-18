e22fca9e5d4455ec8108423025f5c771c1d5ec87
Gabriel Santos Faria <fariamg4@gmail.com>
2025-11-18 10:56:23 -0300

    feat: add shared module exports for interfaces and DTOs

---
+++ meu-projeto-doacao/libs/shared/src/index.ts
@@ -0,0 +1,5 @@
+// Export all shared modules
+export * from './interfaces/message-patterns';
+export * from './interfaces/user.interface';
+export * from './interfaces/error.interface';
+export * from './dto/user.dto';
