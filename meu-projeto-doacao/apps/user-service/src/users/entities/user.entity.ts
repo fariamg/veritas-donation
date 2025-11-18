a6f506bac9c08ef0ac7a7b94f74c9fdc0246a73a
Gabriel Santos Faria <fariamg4@gmail.com>
2025-11-18 10:57:15 -0300

    feat: create UserEntity interface based on Prisma User model

---
+++ meu-projeto-doacao/apps/user-service/src/users/entities/user.entity.ts
@@ -0,0 +1,7 @@
+import { User as PrismaUser } from '../../../generated/prisma';
+
+/**
+ * Interface baseada no modelo User do Prisma
+ * Representa a entidade User com todos os campos
+ */
+export type UserEntity = PrismaUser;
