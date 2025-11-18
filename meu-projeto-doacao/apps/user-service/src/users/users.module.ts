3fab77e19a5c29bc94c9f2728d5e9b7395e4b374
Gabriel Santos Faria <fariamg4@gmail.com>
2025-11-18 10:58:44 -0300

    feat: create UsersModule with controller and service integration

---
+++ meu-projeto-doacao/apps/user-service/src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { PrismaService } from '../prisma.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService, PrismaService],
+  exports: [UsersService], // Exporta para ser usado em outros módulos (ex: AuthModule)
+})
+export class UsersModule {}
