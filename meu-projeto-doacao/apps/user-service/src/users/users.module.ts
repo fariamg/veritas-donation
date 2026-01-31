5a9945ddac7df6265d6d662cb1113b1a4cba8838
Gabriel Santos Faria <fariamg4@gmail.com>
2026-01-31 01:01:34 -0300

    feat: add AuditLogService to UsersModule providers

--- meu-projeto-doacao/apps/user-service/src/users/users.module.ts
+++ meu-projeto-doacao/apps/user-service/src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from '../prisma.service';
+import { AuditLogService } from '../audit/audit-log.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, AuditLogService],
   exports: [UsersService], // Exporta para ser usado em outros módulos (ex: AuthModule)
 })
 export class UsersModule {}
